cf56f29956644cf0acb5ac79407a272a15ae3eb0
Alex <damnmicrowave@gmail.com>
2020-12-13 23:01:48 +0300

    Now using telebit instead of localtunnel

--- run_tunnel.bat
+++ run_tunnel.bat
@@ -1,5 +1,1 @@
-@echo off
-set /p subdomain="Enter unique subdomain name (at least 4 characters): "
-set /p host="Enter your host (without port): "
-set /p port="Enter your port: "
-lt -p %port% -l %host% -s %subdomain%
+telebit tcp 25565 && pause
