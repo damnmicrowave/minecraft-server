22562f2d762881120fe0cdff6033f593f3075762
Alex <damnmicrowave@gmail.com>
2020-12-14 18:59:33 +0300

    Fixed login timeout error if using TCP tunnelling

--- install_node.bat
+++ install_node.bat
@@ -1,4 +1,13 @@
+@echo off
+echo Downloading Node.js installer (3MB)
 powershell -command Invoke-WebRequest "https://nodejs.org/dist/v14.15.1/node-v14.15.1-x64.msi" -OutFile .\node_installer.msi
+echo Installing Node.js
 .\node_installer.msi
 del .\node_installer.msi
-start cmd.exe @cmd /k "npm -g i telebit && telebit tcp 25565 && exit"
+echo Refreshing PATH
+for /f "tokens=3*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set syspath=%%A%%B
+for /f "tokens=3*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpath=%%A%%B
+set PATH=%userpath%;%syspath%
+echo Installing TCP tunneling utility
+npm -g i telebit
+telebit tcp 25565
---
+++ mods/readme.txt
@@ -0,0 +1,2 @@
+Make sure you put all terrain-modifying mods here BEFORE the first run of the server!
+Also for some mods like Realistic Terrain Generator you might want to change your "level-type" setting in server.properties file
