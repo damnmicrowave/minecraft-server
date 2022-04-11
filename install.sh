d0989d2825d0666eb89cb6a71d185abecdc7535e
damnmicrowave <34608186+damnmicrowave@users.noreply.github.com>
2022-04-11 22:39:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#eval "$(/opt/homebrew/bin/brew shellenv)"
+mkdir .homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C .homebrew
+eval "$(.homebrew/bin/brew shellenv)"
 
-#brew install java
-#brew install python
-#/opt/homebrew/bin/pip3 install --user --no-input picomc
-python3 -m picomc account create $USER
+brew install java
+brew install python
+.homebrew/Cellar/python@3.9/3.9.12/bin/pip3 install --user --no-input picomc
+picomc="/Users/${USER}/Library/Python/3.9/bin/picomc"
+$picomc account create $USER
 touch /Applications/Minecraft
-(echo "python3 -m picomc play") > /Applications/Minecraft
+(echo "${picomc} play") > /Applications/Minecraft
 chmod 700 /Applications/Minecraft
